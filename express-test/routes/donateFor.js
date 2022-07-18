0ebd84cbeff97ddb3eb16c249a4a653619bd8bff
ryo0705s <inspire.sasaki15@gmail.com>
2022-07-18 18:58:00 +0900

    postが失敗していますが一旦進みます

--- express-test/routes/donateFor.js
+++ express-test/routes/donateFor.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const res = require("express/lib/response");
 const router = express.Router();
 
 const pool = require("../db/pool");
 
+//データ全件取得API
 router.get("/", function (req, res, next) {
-  // データデータ全権取得API
   pool.query("SELECT * FROM donateFor", function (error, result) {
     if (error) {
       throw error;
@@ -15,11 +14,12 @@ router.get("/", function (req, res, next) {
     });
   });
 });
+
+//データid検索API
 router.get("/:id", function (req, res, next) {
-  // データid検索用API
   const id = req.params.id;
   pool.query(
-    "SELECT * FROM donateFor WHERE id = $1",
+    "SELECT * FROM donateFor where id = $1",
     [id],
     function (error, result) {
       if (error) {
@@ -31,4 +31,31 @@ router.get("/:id", function (req, res, next) {
     }
   );
 });
+
+//データ登録API　追加
+router.post("/", function (req, res, next) {
+  const LENGTH = 10;
+  const SOURCE = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < LENGTH; i++) {
+    result += SOURCE[Math.floor(Math.random() * SOURCE.length)];
+  }
+  const { name, intro } = req.body.donateFor_data;
+  pool.query(
+    "INSERT INTO donateFor VALUES ($1, $2, $3)",
+    [result, name, intro],
+    function (error, result) {
+      if (error) {
+        res.status(500).json({
+          status: "500 Internal Server Error",
+          error: error,
+        });
+      }
+      res.status(201).json({
+        status: "success",
+      });
+    }
+  );
+});
+
 module.exports = router;
