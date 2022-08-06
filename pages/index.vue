32c16ea8ee6f99bf7ed087e4d9c5efabc0388a8a
ryo0705s <inspire.sasaki15@gmail.com>
2022-08-06 17:28:27 +0900

    とりあえず完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container">
     <h1>寄付アプリケーション</h1>
-    <template>
+    <SearchForm />
+    <Table v-bind:getTableData="tableDonateForData" />
+    <!-- <template>
       <el-table :data="tableDonateforData" style="width: 100%">
         <el-table-column prop="id" label="ID" width="180"></el-table-column>
         <el-table-column prop="name" label="Name" width="180"></el-table-column>
         <el-table-column prop="intro" label="Intro"></el-table-column>
       </el-table>
-    </template>
+    </template> -->
   </div>
   <!-- <Tutorial/> -->
 </template>
@@ -40,5 +42,17 @@ export default Vue.extend({
 .container {
   max-width: 1200px;
   margin: 0 auto;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+}
+table th,
+table td {
+  padding: 0.8em;
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
+  text-align: left;
 }
 </style>
