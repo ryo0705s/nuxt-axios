5b037efb17d4404add47eba2e10026531f79557d
ryo0705s <inspire.sasaki15@gmail.com>
2022-08-06 17:29:28 +0900

    とりあえず完成

---
+++ pages/result.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>寄付アプリケーション</h1>
+    <SearchForm />
+    <Table v-bind:getTableData="tableDonateForData"/>
+  </div>
+</template>
+<script>
+import Vue from "vue"
+import DonateForApi from "~/api/index"
+
+export default Vue.extend({
+  data(){
+    return{
+      tableDonateForData:[]
+    }
+  },
+  methods(){
+    this.getIdDonateForData()
+  },
+  watch:{
+    '$root'(to,from){
+      this.getIdDonateForData()
+    }
+  }
+})
+</script>
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
